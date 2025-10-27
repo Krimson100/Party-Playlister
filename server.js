3339654e3be0a997dad14ce5b1c5d2a22adffea4
Thando Breyton Mamukeyani <110921825+Krimson100@users.noreply.github.com>
2025-10-26 23:14:24 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,5 @@
+// --- SPOTIFY CONFIGURATION ---
+const CLIENT_ID = 'a0e5a70475c642a4a68021b0c9dccb52'; // <-- Put your Client ID here
+const CLIENT_SECRET = '38e19ec90c724ae7b0c7b0ce4fd86fc5'; // <-- Put your Client Secret here (KEEP THIS PRIVATE!)
+const SERVICE_USER_ID = 'YOUR_SPOTIFY_SERVICE_USER_ID'; // <-- Put the Spotify User ID of your service account here
+// -----------------------------
